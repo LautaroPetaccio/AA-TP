323fede71c3dd92f7b0ac2145c863bfbd4737c4e
Lautaro Petaccio <lausuper@gmail.com>
2016-09-05 00:46:55 -0300

    Fix de sentiment analysis

--- custom_features.py
+++ custom_features.py
@@ -8,21 +8,23 @@ class SentimentsStats(BaseEstimator, TransformerMixin):
   def fit(self, x, y = None):
     return self
 
-  def transform(self, message_body):
+  def transform(self, messages_bodies):
     sid = SentimentIntensityAnalyzer()
-    body_sentences = tokenize.sent_tokenize(message_body)
-    sentences_stats = map(lambda sentence: sid.polarity_scores(sentence), body_sentences)
-    stats = { 'neg' : 0, 'neu' : 0, 'pos' : 0 }
-    for sentence_stat in sentences_stats:
-      stats['neg'] += sentence_stat['neg']
-      stats['neu'] += sentence_stat['neu']
-      stats['pos'] += sentence_stat['pos']
-
-    stats['neg'] /= len(sentences_stats)
-    stats['neu'] /= len(sentences_stats)
-    stats['pos'] /= len(sentences_stats)
-
-    return stats
+    sentiment_analysis_result = []
+    for message_body in messages_bodies:
+      body_sentences = tokenize.sent_tokenize(message_body)
+      sentences_stats = map(lambda sentence: sid.polarity_scores(sentence), body_sentences)
+      stats = { 'neg' : 0, 'neu' : 0, 'pos' : 0 }
+      for sentence_stat in sentences_stats:
+        stats['neg'] += sentence_stat['neg']
+        stats['neu'] += sentence_stat['neu']
+        stats['pos'] += sentence_stat['pos']
+      if len(sentences_stats) != 0:
+        stats['neg'] /= len(sentences_stats)
+        stats['neu'] /= len(sentences_stats)
+        stats['pos'] /= len(sentences_stats)
+      sentiment_analysis_result.append(stats)
+    return sentiment_analysis_result
 
 class EmailParser(BaseEstimator, TransformerMixin):
   def fit(self, x, y = None):
